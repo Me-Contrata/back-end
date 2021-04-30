a1b45b84dbc5fc2f2a9a65b3fcb2c015811cde8f
Alisson Zampietro <alisson.zampietro@gmail.com>
2021-04-30 10:21:29 +0100

    fix: invalid argument

--- src/v1/app.service.ts
+++ src/v1/app.service.ts
@@ -4,6 +4,6 @@ import { Role } from './common/enums/role.enum';
 @Injectable()
 export class AppService {
   getHello(...roles: Role[]): Array<any> {
-    return ['alisson', 1, 'Maria', {name: 'Alisson'}]
+    return []
   }
 }
